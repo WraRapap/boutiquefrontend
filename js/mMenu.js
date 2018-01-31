1343af48b81e2d8864d0c130240c8d76f0671293
alexmeooow <alexandra96@sina.com>
2018-01-31 15:21:26 +0800

    添加商品喜歡愛心切換效果

--- js/mMenu.js
+++ js/mMenu.js
@@ -40,17 +40,17 @@ jQuery(document).ready(function($){
 	
 
 	//open (or close) submenu items in the lateral menu. Close all the other open submenu items.
-	$('.item-has-children').children('a').on('click', function(event){
-		event.preventDefault();
-		$(this).toggleClass('submenu-open').next('.sub-menu').slideToggle(200).end().parent('.item-has-children').siblings('.item-has-children').children('a').removeClass('submenu-open').next('.sub-menu').slideUp(200);
-	});
-	
-	
-	
-	$('.has-arrow').children('a').on('click', function(event){
-		event.preventDefault();
+    $('.item-has-children').children('a').on('click', function(event){
+        event.preventDefault();
+        $(this).toggleClass('submenu-open').next('.sub-menu').slideToggle(200).end().parent('.item-has-children').siblings('.item-has-children').children('a').removeClass('submenu-open').next('.sub-menu').slideUp(200);
+    });
+
+
+
+    $('.has-arrow').children('a').on('click', function(event){
+        event.preventDefault();
         $(this).toggleClass('submenu-open').next('.filter-submenu').slideToggle(500).end().parent('.has-arrow').siblings('.has-arrow').children('a').removeClass('submenu-open').next('.filter-submenu').slideUp(500);
-	});
+    });
 
 
 });
